babe97a16f55d3fa8d324e4853434ae03abe3341
Lifirenko Bohdan <qp123456ex@gmail.com>
2024-10-30 15:17:45 +0200

    How work Serial.available

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,26 @@
 #include <Arduino.h>
 
+int state = 0;
+
 void setup() {
   pinMode(LED_BUILTIN, HIGH);
+  Serial.begin(9600);
+
+  state = Serial.available();
+  Serial.println(state);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
 
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+    if (state != Serial.available())
+    {
+      state = Serial.available();
+      Serial.println(state);
+    }
+
+    if (state == 10)
+    {
+      String str = Serial.readString();
+      Serial.println(str);
+    }
 }
